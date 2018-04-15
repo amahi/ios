6072853b73303b51ca89ff8200cd7e50e22005e9
Carlos Puchol <cpg+git@amahi.org>
2018-04-15 02:30:41 -0700

    update various things: xcode new version, update pods, fix IQKeyboardManager.sharedManager(), cocoapods

--- AmahiAnywhere/AmahiAnywhere/AppDelegate.swift
+++ AmahiAnywhere/AmahiAnywhere/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        IQKeyboardManager.sharedManager().enable = true
+        IQKeyboardManager.shared.enable = true
         
         self.window? = UIWindow(frame: UIScreen.main.bounds)
         let mainStoryboard: UIStoryboard = UIStoryboard(name: StoryBoardIdentifiers.MAIN, bundle: nil)
