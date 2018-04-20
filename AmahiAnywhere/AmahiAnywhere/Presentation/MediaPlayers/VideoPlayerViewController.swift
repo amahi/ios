b23ec3fb383e4a8a6d4487367c3805abe82141bc
Kanyinsola Fapohunda <lilkaydeee@gmail.com>
2018-04-20 01:10:34 +0100

    feat: Implement video player scrobbling (#86)
    * feat: Implement video player scrobbling

--- AmahiAnywhere/AmahiAnywhere/Presentation/MediaPlayers/VideoPlayerViewController.swift
+++ AmahiAnywhere/AmahiAnywhere/Presentation/MediaPlayers/VideoPlayerViewController.swift
@@ -16,22 +16,30 @@ class VideoPlayerViewController: UIViewController {
     @IBOutlet weak var movieView: UIView!
     @IBOutlet weak var playButton: UIButton!
     @IBOutlet weak var fastForwardButton: UIButton!
-    @IBOutlet weak var fastReverseButton: UIButton!
+    @IBOutlet weak var rewindButton: UIButton!
     @IBOutlet weak var timeLabel: UILabel!
-    @IBOutlet weak var videoControlsStackView: UIStackView!
+    @IBOutlet weak var videoControlsView: UIView!
     @IBOutlet weak var doneButton: UIButton!
     @IBOutlet weak var rewindIndicator: UIImageView!
     @IBOutlet weak var forwardIndicator: UIImageView!
+    @IBOutlet weak var scrobbleSlider: UISlider!
     
     // Set the media url from the presenting Viewcontroller
     private var idleTimer: Timer?
     private var mediaPlayer: VLCMediaPlayer?
     public var mediaURL: URL!
     
+    private var hasMediaFileParseFinished : Bool = false
+    
     fileprivate let JUMP_INTERVAL: Int32 = 15
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let scrobbleTapGesture = UITapGestureRecognizer(target: self, action:  #selector(userTapScrobblePosition(_:)))
+        scrobbleSlider.addGestureRecognizer(scrobbleTapGesture)
+        
+        scrobbleSlider.setThumbImage(UIImage(named: "ic_slider_knob"), for: .normal)
         
         UIApplication.shared.statusBarStyle = .lightContent
         
@@ -44,6 +52,36 @@ class VideoPlayerViewController: UIViewController {
         listenForNotifications()
         self.setUpIndicatorLayers(imageView: rewindIndicator)
         self.setUpIndicatorLayers(imageView: forwardIndicator)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        mediaPlayer = VLCMediaPlayer()
+        mediaPlayer?.delegate = self
+        mediaPlayer?.drawable = self.movieView
+        mediaPlayer?.media = VLCMedia(url: mediaURL)
+        
+        // Play media file immediately after video player launches
+        mediaPlayer?.play()
+        
+        if self.videoControlsView.isHidden {
+            self.videoControlsView.isHidden = false
+        }
+        videoControlsView.superview?.bringSubview(toFront: videoControlsView)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if let _ = mediaPlayer?.isPlaying {
+            mediaPlayer?.stop()
+        }
+        
+        if idleTimer != nil {
+            idleTimer?.invalidate()
+            idleTimer = nil
+        }
     }
     
     func listenForNotifications() {
@@ -82,36 +120,6 @@ class VideoPlayerViewController: UIViewController {
         imageView.alpha = 0.0 // Hide indicator when player opens
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        mediaPlayer = VLCMediaPlayer()
-        mediaPlayer?.delegate = self
-        mediaPlayer?.drawable = self.movieView
-        mediaPlayer?.media = VLCMedia(url: mediaURL)
-        
-        // Play media file immediately after video player launches
-        mediaPlayer?.play()
-        
-        if self.videoControlsStackView.isHidden {
-            self.videoControlsStackView.isHidden = false
-        }
-        videoControlsStackView.superview?.bringSubview(toFront: videoControlsStackView)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        if let _ = mediaPlayer?.media {
-            mediaPlayer?.stop()
-        }
-        
-        if idleTimer != nil {
-            idleTimer?.invalidate()
-            idleTimer = nil
-        }
-    }
-    
     func keepScreenOn(enabled: Bool) {
         UIApplication.shared.isIdleTimerDisabled = enabled
     }
@@ -136,16 +144,16 @@ class VideoPlayerViewController: UIViewController {
         
         idleTimer = nil
         
-        if !videoControlsStackView.isHidden {
-            videoControlsStackView.alpha = 1.0
+        if !videoControlsView.isHidden {
+            videoControlsView.alpha = 1.0
             doneButton.alpha = 1.0
             
             UIView.animate(withDuration: 0.5, delay: 0.0, options: [],
                            animations: {
-                            self.videoControlsStackView.alpha = 0.0
+                            self.videoControlsView.alpha = 0.0
                             self.doneButton.alpha = 0.0
             }) { (completed) in
-                self.videoControlsStackView.isHidden = true
+                self.videoControlsView.isHidden = true
                 self.doneButton.isHidden = true
             }
         }
@@ -153,12 +161,12 @@ class VideoPlayerViewController: UIViewController {
     
     @objc func userTouchScreen() {
         
-        videoControlsStackView.layer.removeAllAnimations()
+        videoControlsView.layer.removeAllAnimations()
         doneButton.layer.removeAllAnimations()
         
-        videoControlsStackView.isHidden = false
+        videoControlsView.isHidden = false
         doneButton.isHidden = false
-        videoControlsStackView.alpha = 1.0
+        videoControlsView.alpha = 1.0
         doneButton.alpha = 1.0
         
         resetTimeAfterStateChanged()
@@ -170,10 +178,6 @@ class VideoPlayerViewController: UIViewController {
             self.resetScreenIdleTimer()
         }
     }
-}
-
-// Mark - VLC Media Player Delegates
-extension VideoPlayerViewController: VLCMediaPlayerDelegate {
     
     @IBAction func rewind(_ sender: Any) {
         mediaPlayer?.jumpBackward(JUMP_INTERVAL)
@@ -210,8 +214,52 @@ extension VideoPlayerViewController: VLCMediaPlayerDelegate {
         }
     }
     
+    @IBAction func userChangedMediaPosition(_ sender: UISlider) {
+        
+        if hasMediaFileParseFinished {
+            let newPosition = VLCTime.init(number: sender.value as NSNumber)
+            timeLabel.text = newPosition?.stringValue
+            mediaPlayer?.position = (newPosition?.value.floatValue)! / (mediaPlayer?.media.length.value.floatValue)!
+            
+        } else {
+            scrobbleSlider.value = 0.0
+        }
+        
+        self.resetScreenIdleTimer()
+    }
+    
+    @objc func userTapScrobblePosition(_ tapGesture: UITapGestureRecognizer) {
+        
+        if scrobbleSlider.isHighlighted {
+            return
+        }
+        let point = tapGesture.location(in: scrobbleSlider)
+        let percentage = point.x / scrobbleSlider.bounds.size.width
+        let delta = Float(percentage) * (scrobbleSlider.maximumValue - scrobbleSlider.minimumValue)
+        let value = scrobbleSlider.minimumValue + delta
+
+        scrobbleSlider.setValue(value, animated: true)
+        
+        if !(mediaPlayer?.isPlaying)! {
+            mediaPlayer?.play()
+        }
+        
+        self.userChangedMediaPosition(scrobbleSlider)
+    }
+    
+    @IBAction func scrobblePositionTouchDown(_ sender: Any) {
+        self.idleTimer?.invalidate()
+        self.idleTimer = nil
+    }
+    
+}
+
+// Mark - VLC Media Player Delegates
+extension VideoPlayerViewController: VLCMediaPlayerDelegate {
+    
     func mediaPlayerTimeChanged(_ aNotification: Notification!) {
         timeLabel.text = mediaPlayer?.time.stringValue
+        scrobbleSlider.value = Float(truncating: (mediaPlayer?.time.value)!)
         
         if mediaPlayer!.time.stringValue == "00:00" {
             self.resetScreenIdleTimer()
@@ -227,14 +275,21 @@ extension VideoPlayerViewController: VLCMediaPlayerDelegate {
             self.dismiss(animated: true, completion: nil)
         } else if mediaPlayer?.state == VLCMediaPlayerState.buffering ||
             mediaPlayer?.state == VLCMediaPlayerState.paused {
+            
+            if !hasMediaFileParseFinished && (mediaPlayer?.media.length.intValue != 0) {
+                
+                scrobbleSlider.maximumValue = Float(truncating: (mediaPlayer?.media.length.value)!)
+                hasMediaFileParseFinished = true
+            }
+            
             self.keepScreenOn(enabled: true)
-            self.videoControlsStackView.isHidden = false
+            self.videoControlsView.isHidden = false
         } else if mediaPlayer?.state == VLCMediaPlayerState.playing {
             self.keepScreenOn(enabled: false)
             self.resetScreenIdleTimer()
         } else {
             self.keepScreenOn(enabled: true)
-            self.videoControlsStackView.isHidden = false
+            self.videoControlsView.isHidden = false
             idleTimer?.invalidate()
             idleTimer = nil
         }
