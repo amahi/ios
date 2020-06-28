aa4ffb2270e65192d1ba02f81a40c7f74af9d964
the-Ripper <43858119+vaibhavbisht06@users.noreply.github.com>
2020-06-28 16:06:43 +0530

    User Agent Header Added (#277)
    * User Agent Header added
    
    Co-authored-by: Vaibhav Bisht <vaibhavbisht@Vaibhavs-MacBook-Air.local>
    Co-authored-by: Shresth Pratap Singh <32341862+ShresthPratapSingh@users.noreply.github.com>

--- AmahiAnywhere/AmahiAnywhere/Utils/Network.swift
+++ AmahiAnywhere/AmahiAnywhere/Utils/Network.swift
@@ -16,12 +16,32 @@ public class Network {
     
     static let shared = Network()
     
-    private func getDefaultHeaders() -> HTTPHeaders {
-        return [
-            "Accept": "application/json"
-            // TODO: Add User-Agent header
-        ]
-    }
+    //eg. iOS/10_1
+       func deviceVersion() -> String {
+           let currentDevice = UIDevice.current
+           return "\(currentDevice.systemName)/\(currentDevice.systemVersion)"
+       }
+       //eg. iPhone5,2
+       func deviceName() -> String {
+           var sysinfo = utsname()
+           uname(&sysinfo)
+           return String(bytes: Data(bytes: &sysinfo.machine, count: Int(_SYS_NAMELEN)), encoding: .ascii)!.trimmingCharacters(in: .controlCharacters)
+       }
+       //eg. MyApp/1
+       func appNameAndVersion() -> String {
+           let dictionary = Bundle.main.infoDictionary!
+           let version = dictionary["CFBundleShortVersionString"] as! String
+           let name = dictionary["CFBundleName"] as! String
+           return "\(name)/\(version)"
+       }
+       
+       private func getDefaultHeaders() -> HTTPHeaders {
+           return [
+               "Accept": "application/json" +
+               "\(appNameAndVersion()) \(deviceName()) \(deviceVersion())"
+           ]
+       }
+
     
     private func getFinalHeaders(_ headers: HTTPHeaders) -> HTTPHeaders {
         var finalHeaders = getDefaultHeaders()
